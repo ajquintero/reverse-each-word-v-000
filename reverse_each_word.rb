9872e69cffa016ed2bc82e5784fedffd9b346749
Abel Quintero <abel.j.quintero@gmail.com>
2018-03-14 23:54:09 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 sentence = "Hello, how are you?"
 def reverse_each_word(sentence)
-  reversed = sentence.split("").each do |words|
-    words.reverse  
+  reversed = sentence.split(" ").collect do |words|
+    words.reverse
   end
 end
