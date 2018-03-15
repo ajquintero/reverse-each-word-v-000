cb57b34f1f4a638e2a9e918feb151ce85d9182bf
Abel Quintero <abel.j.quintero@gmail.com>
2018-03-15 00:22:43 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,16 @@
 sentence = "Hello there, and how are you?"
+
 def reverse_each_word(sentence)
-  reversed = sentence.split(" ").collect do |words|
-    words.reverse
+  reversed_sentence = []
+  sentence.split.each do |word|
+    reversed_sentence << word.reverse
   end
-    reversed.join(" ")
+  reversed_sentence
 end
+# #using collect 
+# def reverse_each_word(sentence)
+#   reversed = sentence.split(" ").collect do |words|
+#     words.reverse
+#   end
+#     reversed.join(" ")
+# end
