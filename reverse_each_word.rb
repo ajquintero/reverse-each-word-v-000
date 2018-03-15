0226b90b9707eff86fec85452d4abd3f80fdf6ac
Abel Quintero <abel.j.quintero@gmail.com>
2018-03-15 00:22:55 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,9 +5,9 @@ def reverse_each_word(sentence)
   sentence.split.each do |word|
     reversed_sentence << word.reverse
   end
-  reversed_sentence
+  reversed_sentence.join
 end
-# #using collect 
+# #using collect
 # def reverse_each_word(sentence)
 #   reversed = sentence.split(" ").collect do |words|
 #     words.reverse
